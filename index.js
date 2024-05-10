2942b1fcbb32368365ebbbf3b62e2720e9087ffd
Carlosbolanorua <daniel14agosto@hotmail.com>
2024-05-10 15:42:01 -0500

    cambios innecesarios

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http")
 
 function requestController(){
     //Logica de la funcion
-    console.log("Hola mundo!")
+    console.log("Hola mundo!!")
 }
 //Configurar servidor
 const server = http.createServer(requestController)
