5ad803a5951d95a34a408a5d3d38d11edd119984
Carlosbolanorua <daniel14agosto@hotmail.com>
2024-05-10 15:50:19 -0500

    innecesario2.0

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const http = require("http")
 
 function requestController(){
     //Logica de la funcion
-    console.log("Hola mundo!!")
+    console.log("Hola mundo!!");
 }
 //Configurar servidor
 const server = http.createServer(requestController)
 
 const PORT = process.env.PORT
 server.listen(PORT,function (){
-    console.log("Puerto:"+PORT);
+    console.log("Puerto:"+PORT)
 })
