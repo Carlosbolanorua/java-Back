f75e2cf588f618e08e0613978a2b033df5c279f4
Carlosbolanorua <daniel14agosto@hotmail.com>
2024-05-11 21:55:25 -0500

    agregando headers y mensaje ene la pantalla principal

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 require("dotenv").config()
 const http = require("http")
 
-function requestController(){
+function requestController(req,res){
     //Logica de la funcion
-    console.log("Hola mundo!!");
+    const url = req.url
+    const method = req.method
+    console.log({url,method})
+
+    if(method==="GET" && url==="/"){
+        res.setHeader("Content-type","text/html; charset=utf-8")
+        res.write("<h1>Hola mundo desde la pagina Principal</h1>")
+        res.end()
+        return
+    }
+    
+    if(method==="GET" && url==="/about"){
+        
+    }
+
 }
 //Configurar servidor
 const server = http.createServer(requestController)
 
 const PORT = process.env.PORT
+
 server.listen(PORT,function (){
-    console.log("Puerto:"+PORT)
+    console.log("Puerto: "+PORT)
 })
